a1b05b406736688d9449dae8089db8ce0c85a31a
vcolavin <vcolavin@gmail.com>
2019-05-21 22:26:03 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5394224bec059f95836e.js"
+    "url": "webpack-runtime-0fb04b745f56178ca54c.js"
   },
   {
     "url": "styles.fad166f8e9bfc76c5113.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b7eb3f8214582dc34de9009e07044777"
+    "revision": "6c4d9fe69ebf9ac8d8e38313c72a642c"
   },
   {
     "url": "component---src-pages-404-js-f15049531d3245e73c12.js"
